5499bb748216d71221d2a90948f8fba09f13a056
Anders Ballegaard <anderstb@hotmail.dk>
2019-05-30 17:39:54 +0200

    Black formatting

--- demo_client/client.py
+++ demo_client/client.py
@@ -6,36 +6,33 @@ import uuid
 
 iid = uuid.uuid1()
 
+
 def get_lock(lock_name, host="127.0.0.1"):
-    r = {
-        "active": False,
-        "hello" : 0,
-        "dead" : 0
-    }
+    r = {"active": False, "hello": 0, "dead": 0}
 
     try:
-        lock_resp = requests.get(f"http://{host}:5050/lock?name={lock_name}&instance={iid}")
+        lock_resp = requests.get(
+            f"http://{host}:5050/lock?name={lock_name}&instance={iid}"
+        )
         r1 = lock_resp.json()
-        #print(r1)
+        # print(r1)
         if "active" in r1.keys() or "hello" in r1.keys() or "dead" in r1.keys():
             r = r1
     except Exception as e:
         print("Encountered error: " + e)
-    
+
     return r
 
+
 is_active = False
-lock = {
-        "active": False,
-        "hello" : 1,
-        "dead" : 0
-    }
+lock = {"active": False, "hello": 1, "dead": 0}
+
 
 def lock_thread(lock_name="default"):
     global lock
     global is_active
     while True:
-        #print("Running")
+        # print("Running")
         l = get_lock(lock_name)
         lock = l
         is_active = l["active"]
